6312e05c0c00d5c109fe4fa664cf6b6e895f8e4e
Geoffrey <dotrey@gmail.com>
2020-01-12 16:17:11 +0100

    updated qr ghost

--- qr/built/qrGhost.js
+++ qr/built/qrGhost.js
@@ -258,13 +258,14 @@ export default class qrGhost {
             let selectButton = document.getElementById("select-device-button");
             if (devices.length > 1) {
                 this.selectDeviceContainer.innerHTML = "";
+                let i = 1;
                 for (let device of devices) {
                     let d = document.createElement("div");
                     d.classList.add("device");
                     if (device.deviceId === activeDeviceId) {
                         d.classList.add("active");
                     }
-                    d.innerHTML = device.label;
+                    d.innerHTML = "Camera " + i++;
                     this.addClickListener(d, (e) => {
                         this.backStack.pop();
                         this.selectDevice(device.deviceId);
